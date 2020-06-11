de081c853de62fa195b6059f2ede71996727570f
Anthony George <ageorge530@gmail.com>
2020-06-11 06:05:06 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,35 @@
 class Appointment 
+  attr_accessor :name, :genre
+  
+  @@all =[]
+  
+  
+  def initialize(name)
+    @name = name
+    @genre = genre
+    @@all << self 
+  end  
+    
+   def self.all
+     @@all 
+    end
+    
+  def songs  
+    Song.all.select { |song|song.artist == self}
+  end
+  
+  def new_song(name, genre)
+    Song.new(name,self,genre)
+  end
+  
+  def genres  
+    songs.collect{ |song|song.genre}
+  end
+  
+  
+  
+end     
+  
+  
+    
+    
