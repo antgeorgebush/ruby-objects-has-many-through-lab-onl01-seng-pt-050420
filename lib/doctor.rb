a0095c73dbf81c4f3b966f85611199281804fec2
Anthony George <ageorge530@gmail.com>
2020-06-11 05:54:51 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor
+  attr_accessor :name, :genre
+  
+  @@all =[]
+  
+  
+  def initialize(name)
+    @name = name
+    @genre = genre
+    @@all << self 
+  end  
+    
+   def self.all
+     @@all 
+    end
+    
+  def songs  
+    Song.all.select { |song|song.artist == self}
+  end
+  
+  def new_song(name, genre)
+    Song.new(name,self,genre)
+  end
+  
+  def genres  
+    songs.collect{ |song|song.genre}
+  end
+  
+  
+  
+end     
+  
